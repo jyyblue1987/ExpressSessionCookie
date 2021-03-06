7a8243eae55c8e29f647e028bcf0c339d8dcfa40
jyyblue <jyyblue1987@outlook.com>
2021-03-06 17:59:40 +0800

    [add] add part6. session.

--- src/movies/app.js
+++ src/movies/app.js
@@ -1,28 +1,47 @@
 // app.js
 const express = require('express');
 const app = express();
+var session = require('express-session');
+
 const Movie = require('./db');
 
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/movie', function(req, res) {
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+
+app.get('/', function(req, res) {
+    res.redirect('/movies');    
+});
+
+app.get('/movies', function(req, res) {
     var query = req.query;    
     if( !query.director )
         query = {};
 
     Movie.find(query, function(err, varToStoreResult, count) {
-        console.log(varToStoreResult); // <---- variable contains found documents!
-
         res.render('movie', {list:varToStoreResult, director:query['director']});
     });
 });
 
-app.get('/movie/add', function(req, res) {
+app.get('/mymovies', function(req, res) {
+    if( !req.session.mymovies )
+        req.session.mymovies = [];
+
+    res.render('mymovie', {list:req.session.mymovies});
+});
+
+app.get('/movies/add', function(req, res) {
     res.render('movie.add.hbs', {});    
 });
 
-app.post('/movie/add', function(req, res) {
+app.post('/movies/add', function(req, res) {
     var data = req.body;    
 
     console.log(JSON.stringify(data));
@@ -31,8 +50,14 @@ app.post('/movie/add', function(req, res) {
     // your request handler that deals with POSTs will create a new movie in the database
     Movie.create(data, function (err, awesome_instance) {
         if (err) return handleError(err);
+
+        if( !req.session.mymovies )
+            req.session.mymovies = [];
         
-        res.redirect('/movie');    
+        req.session.mymovies.push(data);
+        console.log(JSON.stringify(req.session.mymovies));
+        
+        res.redirect('/movies');    
     });
 });
 
